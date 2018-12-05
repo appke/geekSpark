db57108bd505426c529a3559c800239895957408
muliang001 <anglimu@163.com>
2018-12-05 18:02:57 +0800

    MODIFY: 修改readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # geekSpark
-🌠Spark
+- Scala环境搭建
+
+
+
+
+
+
+
+## Scala
+- [Scala入门笔记](https://www.jianshu.com/p/d76a03e22556)
+- [Scala类和对象](https://www.jianshu.com/p/8a216c71e125)
+
+
+## Spark🌠
---
+++ helloworld/HelloWorld.scala
@@ -0,0 +1,17 @@
+object HelloWorld {
+	def main(args:Array[String]) {
+		println("Hello world!！！！");
+	}
+}
+
+
+// object MyStrTest {
+// 	def mystr(name: String): String= {
+// 		s"my name is $name"
+// 	}
+
+// 	def main(args: Array[String]): Unit = {
+// 		println(mystr("2018-11-23"))
+// 	}
+// }
+
